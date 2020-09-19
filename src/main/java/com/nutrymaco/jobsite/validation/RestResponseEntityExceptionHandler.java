306b6c4a5677ab25155437a3573642970ed1c72f
Nutrymaco <efim7299@icloud.com>
2020-09-19 21:24:57 +0300

    add error handling for POST /vacancies

---
+++ src/main/java/com/nutrymaco/jobsite/validation/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.nutrymaco.jobsite.validation;
+
+import com.nutrymaco.jobsite.exception.validation.VacancyValidationException;
+import com.nutrymaco.jobsite.exception.validation.ValidationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {VacancyValidationException.class})
+    protected ResponseEntity<Object> handleConflict(Exception ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(),
+                new HttpHeaders(), HttpStatus.EXPECTATION_FAILED, request);
+    }
+}
