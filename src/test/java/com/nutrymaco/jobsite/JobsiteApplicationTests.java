410fae567c0c82b95ce5e179d3ffe2c8fb78937f
Nutrymaco <efim7299@icloud.com>
2020-11-07 17:22:34 +0300

    add all for deploy

--- src/test/java/com/nutrymaco/jobsite/JobsiteApplicationTests.java
+++ src/test/java/com/nutrymaco/jobsite/JobsiteApplicationTests.java
@@ -1,13 +1,15 @@
 package com.nutrymaco.jobsite;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JobsiteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testTest() {
+		assertTrue(true);
 	}
 
 }
